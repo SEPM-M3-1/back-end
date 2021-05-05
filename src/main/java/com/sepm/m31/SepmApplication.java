e1a6d4310d06777aa2a5d9185be4d1e6694fcd49
Lawrence Liao <Lawrence.liao.au@gmail.com>
2021-05-05 11:42:26 +1000

    Push the skeleton code

---
+++ src/main/java/com/sepm/m31/SepmApplication.java
@@ -0,0 +1,13 @@
+package com.sepm.m31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SepmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SepmApplication.class, args);
+    }
+
+}
