bd28a4d10bb5d8a15e89fe0669033d06d72f6b9b
AisinAllin <AisinAllin@gmail.com>
2021-05-12 19:45:01 +1000

    test commit message+

--- src/main/java/com/sepm/controllers/UserController.java
+++ src/main/java/com/sepm/controllers/UserController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-//test
+//test123
     private final ManagerService managerService;
     private final StaffService staffService;
 
